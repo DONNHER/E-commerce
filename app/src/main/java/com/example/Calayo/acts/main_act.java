8393ee7052e9757405822df8796a5ab430bece56
Meeepp <s.elardo.edward@cmu.edu.ph>
2025-05-08 19:06:37 +0800

    update

--- app/src/main/java/com/example/Calayo/acts/main_act.java
+++ app/src/main/java/com/example/Calayo/acts/main_act.java
@@ -17,7 +17,6 @@ import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -34,31 +33,29 @@ import java.util.Arrays;
 import java.util.List;
 
 public class main_act extends AppCompatActivity {
-//    private RecyclerView appointmentsView ;
-//    private order_adaptor Adapt;
     private Button btn1, btn2;
     private final ArrayList<Order> orders = new ArrayList<>();
     private RecyclerView recyclerView;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
-    private final FirebaseAuth myAuth= FirebaseAuth.getInstance();
+    private final FirebaseAuth myAuth = FirebaseAuth.getInstance();
 
-    private RecyclerView products ;
+    private RecyclerView products;
     private product_adapt Adapt;
 
     RecyclerView addsRecyclerView;
     AddsADaptor adapter;
 
     @Override
-    public void onStart(){
+    public void onStart() {
         super.onStart();
         FirebaseUser user = myAuth.getCurrentUser();
-        if(user != null){
+        if (user != null) {
             String uid = user.getUid();
             db.collection("users").document(uid).get().addOnSuccessListener(documentSnapshot -> {
-                if(documentSnapshot.exists()) {
-                        Intent intent = new Intent(this, UserDashboardAct.class); // Replace with actual target
-                        startActivity(intent);
-                        finish();
+                if (documentSnapshot.exists()) {
+                    Intent intent = new Intent(this, UserDashboardAct.class);
+                    startActivity(intent);
+                    finish();
                 }
             });
         }
@@ -68,38 +65,42 @@ public class main_act extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        recyclerView = findViewById(R.id.appointmentsView);
-//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+
         ImageView home = findViewById(R.id.home);
         home.setOnClickListener(view -> {
-            Intent homepage = new Intent(this,UserDashboardAct.class);
+            Intent homepage = new Intent(this, UserDashboardAct.class);
             startActivity(homepage);
         });
+
         ImageView menu = findViewById(R.id.foodMenu);
         menu.setOnClickListener(view -> {
-            Intent menupage = new Intent(this,productsAct.class);
+            Intent menupage = new Intent(this, productsAct.class);
             startActivity(menupage);
         });
-//        ImageView history = findViewById(R.id.history);
-//        menu.setOnClickListener(view -> {
-//            Intent menupage = new Intent(this,productsAct.class);
-//            startActivity(menupage);
-//        });
+
+        // ImageView history = findViewById(R.id.history);
+        // menu.setOnClickListener(view -> {
+        //     Intent menupage = new Intent(this, productsAct.class);
+        //     startActivity(menupage);
+        // });
+
         ImageView profile = findViewById(R.id.profile);
         profile.setOnClickListener(view -> {
             Intent profilepage = new Intent(this, settingAct.class);
             startActivity(profilepage);
         });
-        //Products
+
         products = findViewById(R.id.Products_Recycler);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         products.setLayoutManager(layoutManager);
 
         ArrayList<Item> data = getItems();
-        Adapt = new product_adapt(data,this);
+        Adapt = new product_adapt(data, this);
         products.setAdapter(Adapt);
 
-        // adds
         addsRecyclerView = findViewById(R.id.Adds_Recycler);
         LinearLayoutManager layoutManager2 = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
         addsRecyclerView.setLayoutManager(layoutManager2);
@@ -107,24 +108,29 @@ public class main_act extends AppCompatActivity {
         SnapHelper snapHelper = new LinearSnapHelper();
         snapHelper.attachToRecyclerView(addsRecyclerView);
 
-        List<adds> data2 = Arrays.asList(new adds("Get 40% off\nYour First Order"),new adds("Get 10% off\nYour First Order"),new adds("Get 30% off\nYour First Order"),new adds("Get 20% off\nYour First Order"));
-        adapter = new AddsADaptor(data2,this);
+        List<adds> data2 = Arrays.asList(
+            new adds("Get 40% off\nYour First Order"),
+            new adds("Get 10% off\nYour First Order"),
+            new adds("Get 30% off\nYour First Order"),
+            new adds("Get 20% off\nYour First Order")
+        );
+        adapter = new AddsADaptor(data2, this);
         addsRecyclerView.setAdapter(adapter);
     }
 
     @NonNull
     private static ArrayList<Item> getItems() {
         ArrayList<Item> data = new ArrayList<>();
-        Item newItem = new Item(200,"https://ik.imagekit.io/k4imggmfa/pizza1.png?updatedAt=1746566552587","Pizza",50,"Hawaiian pizza typically uses, mozzarella cheese, ham pizza dough, pizza sauce, mozzarella cheese, ham, and pineapple. Some variations also include bacon or other toppings.");
-        Item newItem2 = new Item(180,"https://ik.imagekit.io/k4imggmfa/burger.png?updatedAt=1746566830867","Burger",50,"Hawaiian pizza typically uses, mozzarella cheese, ham pizza dough, pizza sauce, mozzarella cheese, ham, and pineapple. Some variations also include bacon or other toppings.");
-        Item newItem3 = new Item(250,"https://ik.imagekit.io/k4imggmfa/hotdogs.png?updatedAt=1746566840760","Chicken Burger",50,"Hawaiian pizza typically uses, mozzarella cheese, ham pizza dough, pizza sauce, mozzarella cheese, ham, and pineapple. Some variations also include bacon or other toppings.");
+        Item newItem = new Item(200, "https://ik.imagekit.io/k4imggmfa/pizza1.png?updatedAt=1746566552587", "Pizza", 50, "Hawaiian pizza typically uses, mozzarella cheese, ham pizza dough, pizza sauce, mozzarella cheese, ham, and pineapple. Some variations also include bacon or other toppings.");
+        Item newItem2 = new Item(180, "https://ik.imagekit.io/k4imggmfa/burger.png?updatedAt=1746566830867", "Burger", 50, "Hawaiian pizza typically uses, mozzarella cheese, ham pizza dough, pizza sauce, mozzarella cheese, ham, and pineapple. Some variations also include bacon or other toppings.");
+        Item newItem3 = new Item(250, "https://ik.imagekit.io/k4imggmfa/hotdogs.png?updatedAt=1746566840760", "Chicken Burger", 50, "Hawaiian pizza typically uses, mozzarella cheese, ham pizza dough, pizza sauce, mozzarella cheese, ham, and pineapple. Some variations also include bacon or other toppings.");
 
         data.add(newItem);
         data.add(newItem2);
         data.add(newItem3);
-        Item newI= new Item(200,"https://ik.imagekit.io/k4imggmfa/pizza1.png?updatedAt=1746566552587","Pizza",50,"Hawaiian pizza typically uses, mozzarella cheese, ham pizza dough, pizza sauce, mozzarella cheese, ham, and pineapple. Some variations also include bacon or other toppings.");
-        Item newIte = new Item(200,"https://ik.imagekit.io/k4imggmfa/burger.png?updatedAt=1746566830867","Burger",50,"Hawaiian pizza typically uses, mozzarella cheese, ham pizza dough, pizza sauce, mozzarella cheese, ham, and pineapple. Some variations also include bacon or other toppings.");
-        Item neww = new Item(200,"https://ik.imagekit.io/k4imggmfa/hotdogs.png?updatedAt=1746566840760","Chicken Burger",50,"Hawaiian pizza typically uses, mozzarella cheese, ham pizza dough, pizza sauce, mozzarella cheese, ham, and pineapple. Some variations also include bacon or other toppings.");
+        Item newI = new Item(200, "https://ik.imagekit.io/k4imggmfa/pizza1.png?updatedAt=1746566552587", "Pizza", 50, "Hawaiian pizza typically uses, mozzarella cheese, ham pizza dough, pizza sauce, mozzarella cheese, ham, and pineapple. Some variations also include bacon or other toppings.");
+        Item newIte = new Item(200, "https://ik.imagekit.io/k4imggmfa/burger.png?updatedAt=1746566830867", "Burger", 50, "Hawaiian pizza typically uses, mozzarella cheese, ham pizza dough, pizza sauce, mozzarella cheese, ham, and pineapple. Some variations also include bacon or other toppings.");
+        Item neww = new Item(200, "https://ik.imagekit.io/k4imggmfa/hotdogs.png?updatedAt=1746566840760", "Chicken Burger", 50, "Hawaiian pizza typically uses, mozzarella cheese, ham pizza dough, pizza sauce, mozzarella cheese, ham, and pineapple. Some variations also include bacon or other toppings.");
 
         data.add(newItem);
         data.add(newItem2);
@@ -133,22 +139,21 @@ public class main_act extends AppCompatActivity {
         data.add(newIte);
         data.add(neww);
         return data;
-
     }
 
     public void onMenuClick2(View view) {
         Intent intent = new Intent(view.getContext(), settingAct.class);
         view.getContext().startActivity(intent);
-
     }
+
     public void onLogClick(View view) {
         Intent intent = new Intent(view.getContext(), userLoginAct.class);
         view.getContext().startActivity(intent);
     }
+
     public void onResClick(View view) {
         userRegisterAct dialogFragment = new userRegisterAct();
         Intent intent = new Intent(view.getContext(), userLoginAct.class);
         view.getContext().startActivity(intent);
     }
-
 }
